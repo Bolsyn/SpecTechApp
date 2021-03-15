3aa96dbc34e7d40c802e833c92b08a1a41bb134c
Bolsyn <Serikagis@yandex.ru>
2021-03-15 19:48:25 +0600

    Add project files.

---
+++ SpecTechApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpecTechApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
